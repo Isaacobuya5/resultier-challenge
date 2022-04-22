328778a57de2ed2d4860d0eb72fa356ae2322681
Isaac Obuya <Szack4477@gmail.com>
2022-04-22 11:02:12 +0300

    fix some issues, add header and footer to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { Fragment } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
 export default function Home() {
-  return <div className="container">Hello</div>;
+  return (
+    <div className="container">
+    </div>
+  );
 }
