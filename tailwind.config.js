24b55b24736d6c42164560beff7135d36e7b9309
Isaac Obuya <Szack4477@gmail.com>
2022-04-22 09:34:06 +0300

    setting up the color and font themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      Barlow: ["Barlow Semi Condensed", "sans-serif"],
+    },
+    extend: {
+      scissorsGradientFrom: "hsl(39, 89%, 49%)",
+      scissorsGradientTo: "hsl(40, 84%, 53%)",
+      paperGradientFrom: "hsl(230, 89%, 62%)",
+      paperGradientTo: "hsl(230, 89%, 65%)",
+      rockGradientFrom: "hsl(349, 71%, 52%)",
+      rockGradientTo: "hsl(349, 70%, 56%)",
+      lizardGradientFrom: "hsl(261, 73%, 60%)",
+      lizardGradientTo: "hsl(261, 72%, 63%)",
+      cyanGradientFrom: "hsl(189, 59%, 53%)",
+      cyanGradientTo: "hsl(189, 58%, 57%)",
+      darkText: "hsl(229, 25%, 31%)",
+      scoreText: "hsl(229, 64%, 46%)",
+      headerOutline: "hsl(217, 16%, 45%)",
+      radialGradientFrom: "hsl(214, 47%, 23%)",
+      radialGradientTo: "hsl(237, 49%, 15%)",
+    },
+    screens: {
+      mobile: "375px",
+      desktop: "1366px",
+    },
   },
   plugins: [],
-}
+};
